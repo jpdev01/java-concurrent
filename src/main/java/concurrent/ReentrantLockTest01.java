0c1e77e5e736b91297b078e1f78b66d60c416ff1
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2023-06-01 13:27:17 -0300

    ReentrantLock

---
+++ src/main/java/concurrent/ReentrantLockTest01.java
@@ -0,0 +1,48 @@
+package concurrent;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * tryLock: pode tentar esperar o lock por x segundos. Caso não consiga continua a execução
+ * synchronize basicamente trava e a thread fica wait until.
+ * Possibilidade de interromper thread que está esperando pelo Lock
+ * Quando trabalha manualmente com lock, tem que ter certeza que vai liberar, ou terá memoryLeak e fará o app travar. - lock.unlock()
+ */
+class Worker implements Runnable {
+    private String name;
+    private ReentrantLock lock;
+
+    public Worker(String name, ReentrantLock lock) {
+        this.name = name;
+        this.lock = lock;
+    }
+
+    @Override
+    public void run() {
+        lock.lock();
+        try {
+            System.out.printf("Thread %s entrou em uma sessão crítica%n", name);
+            System.out.printf("%d Threads esperando na fila%n", lock.getQueueLength());
+            System.out.printf("Thread %s vai esperar 2s%n", name);
+            Thread.sleep(2000);
+            System.out.printf("Thread %s finalizou a espera%n", name);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
+
+class ReentrantLockTest01 {
+    public static void main(String[] args) {
+        ReentrantLock lock = new ReentrantLock(true);
+        new Thread(new Worker("A", lock)).start();
+        new Thread(new Worker("B", lock)).start();
+        new Thread(new Worker("C", lock)).start();
+        new Thread(new Worker("D", lock)).start();
+        new Thread(new Worker("E", lock)).start();
+        new Thread(new Worker("F", lock)).start();
+        new Thread(new Worker("G", lock)).start();
+    }
+}
